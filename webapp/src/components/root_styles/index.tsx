45e5bc912dfe7e394b483071caa79afa2e9b50c0
naka <235416384+akinakamu@users.noreply.github.com>
2026-09-04 17:48:24 +0900

    feat: dynamically replace audio device description based on user locale

--- webapp/src/components/root_styles/index.tsx
+++ webapp/src/components/root_styles/index.tsx
@@ -1,8 +1,28 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import type * as React from 'react';
+import type React from 'react';
+import {useEffect} from 'react';
+import {useSelector} from 'react-redux';
+
+import type {GlobalState} from '@mattermost/types/store';
+
 import './styles.css';
 
-const RootStyles: React.FC = () => null;
+const RootStyles: React.FC = () => {
+    const currentLocale = useSelector((state: GlobalState) => {
+        const currentUserId = state.entities?.users?.currentUserId;
+        return (currentUserId && state.entities?.users?.profiles?.[currentUserId]?.locale) || 'ja';
+    });
+
+    useEffect(() => {
+        if (currentLocale) {
+            document.documentElement.setAttribute('data-inject-css-locale', currentLocale);
+        }
+    }, [currentLocale]);
+
+    return null;
+};
+
 export default RootStyles;
+
